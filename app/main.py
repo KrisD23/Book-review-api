4507c336a9ccac78db0d58e06971d54563375f79
KrisD23 <krishandwive9@protonmail.com>
2026-08-01 17:11:56 +0530

    feat: add post and get routes for the books

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
+
 app = FastAPI(
     title="Book Review API",
     version="0.1.0",
     description="A REST API for managing books and reviews.",
 )
+
 
 @app.get(
     "/",
@@ -13,6 +15,44 @@ app = FastAPI(
 )
 async def root():
     return {"message": "Book Review API"}
+
+
+books = [
+    {
+        "id": 1,
+        "title": "Atomic Habits",
+        "author": "James Clear",
+    },
+    {
+        "id": 2,
+        "title": "Clean Code",
+        "author": "Robert C. Martin",
+    },
+]
+
+@app.get(
+    "/books",
+    tags=["Books"],
+    summary="Get all books",
+)
+async def get_books():
+    return books
+
+
+@app.post(
+    "/books",
+    tags=["Books"],
+    summary="Add a new book",
+    status_code=201
+)
+async def add_book(book:dict):
+    new_book = {
+        "id": len(books) + 1,
+        "title": book["title"],
+        "author": book["author"],
+    }
+    books.append(new_book)
+    return new_book
 
 
 @app.get(
