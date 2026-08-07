081cd1dee2728b8810ae590a1367b81f868ab655
KrisD23 <krishandwive9@protonmail.com>
2026-08-08 00:10:18 +0530

    feat: enhance user registration with response model and error handling

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from dependencies.database import get_db
-from schemas.user import UserCreate
+from schemas.user import UserCreate, UserResponse
 from services.auth_service import create_user
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-@router.post("/register")
+@router.post("/register", response_model=UserResponse, status_code=201)
 async def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(user, db)
    
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,8 +1,10 @@
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError,IntegrityError
 from sqlalchemy.orm import Session
 from schemas.user import UserCreate
 from models.user import User
 from utils.security import hash_password
+from fastapi import HTTPException
+
 
 def create_user(user: UserCreate, db: Session):
     hashed_password = hash_password(user.password)
@@ -10,6 +12,11 @@ def create_user(user: UserCreate, db: Session):
     db.add(new_user)
     try:
         db.commit()
+
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Email already exists")
+    
     except SQLAlchemyError:
         db.rollback()
         raise
