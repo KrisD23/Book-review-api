8540e56f5cb30fead939b9fc2b211baace3a4d41
KrisD23 <krishandwive9@protonmail.com>
2026-08-08 10:27:27 +0530

    feat: update login user endpoint to use OAuth2PasswordRequestForm for authentication

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from dependencies.database import get_db
-from schemas.user import UserCreate, UserLogin, UserResponse
+from schemas.user import UserCreate, UserResponse
 from services.auth_service import create_user
 from services.auth_service import login_user as login_user_service
 
@@ -14,5 +15,5 @@ async def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(user, db)
 
 @router.post("/login")
-async def login_user(user: UserLogin, db: Session = Depends(get_db)):
-    return login_user_service(user.email, user.password, db)
+async def login_user( db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    return login_user_service(form_data.username, form_data.password, db)
